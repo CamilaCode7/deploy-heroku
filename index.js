4a0240999c911bd05d89aaaee90a28ef1d3346f1
CamilaCode7 <camilafabiabh9@gmail.com>
2021-11-16 22:33:20 -0300

    atualizando index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    req.statusCode(200).send('Esta vivo')  
+    req.statusCode(200).send('Esta vivo!!!')  
 });
 
 const PORT = process.env.PORT || 3000;
