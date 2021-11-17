9d6c102011234156288586be3aa1339a5871a1e5
CamilaCode7 <camilafabiabh9@gmail.com>
2021-11-16 22:09:59 -0300

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
 app.get('/', (req, res) => {
-  req.statusCode(200).send('Esta vivo')  
+    req.statusCode(200).send('Esta vivo')  
 });
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
